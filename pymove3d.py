e5571d382f1c041e2e3d8a0992ec2b07c157d7dc
Peter Koppatz <peter@koppatz.com>
2013-11-27 13:09:28 +0100

    Hightlight active menu

--- pymove3d.py
+++ pymove3d.py
@@ -21,7 +21,7 @@ def get_locale():
 
 @app.route("/index")
 def index():
-    return render_template("index.html", act="index")
+    return render_template("index.html")
 
 @app.route("/competition")
 def competition():
