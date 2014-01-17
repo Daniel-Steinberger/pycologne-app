9df159ebc3f8800de64a12e3f39d2db3250c8df3
Reimar Bauer <rb.proj@gmail.com>
2014-01-17 18:38:52 +0100

    missing 404 page added

--- pymove3d.py
+++ pymove3d.py
@@ -53,7 +53,7 @@ def privacy():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template(_("en/404.html")), 404
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5014, debug=True)
