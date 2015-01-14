27d50a13aed28a3d391f2425650aa8df0c2d5555
Rebecca Breu <rebecca@rbreu.de>
2015-01-14 21:02:14 +0100

    Uebersetzungen in sayings enfernt; index.html gefixt

--- sayings.py
+++ sayings.py
@@ -1,29 +1,28 @@
-from flask.ext.babel import gettext as _
 from random import shuffle
 
 def get_saying():
-    sayings = [(_(u'The Zen of Python, by Tim Peters'), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Beautiful is better than ugly."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Explicit is better than implicit."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Simple is better than complex."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Complex is better than complicated."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Flat is better than nested."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Sparse is better than dense."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Readability counts."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Special cases aren't special enough to break the rules."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Although practicality beats purity."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Errors should never pass silently, unless explicitly silenced."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"In the face of ambiguity, refuse the temptation to guess."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Although that way may not be obvious at first unless you're Dutch."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Now is better than never."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Although never is often better than *right* now."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"If the implementation is hard to explain, it's a bad idea."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"If the implementation is easy to explain, it may be a good idea."), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"Namespaces are one honking great idea -- let's do more of those!"), _(u'__Tim Peters, The Zen of Python')),
-               (_(u"I once tried Java, but it was too complicated for me, Python is easier."),_(u'__Valentin Pratz,  novice programmers')),
+    sayings = [(u'The Zen of Python, by Tim Peters', u'__Tim Peters, The Zen of Python'),
+               (u"Beautiful is better than ugly.", u'__Tim Peters, The Zen of Python'),
+               (u"Explicit is better than implicit.", u'__Tim Peters, The Zen of Python'),
+               (u"Simple is better than complex.", u'__Tim Peters, The Zen of Python'),
+               (u"Complex is better than complicated.", u'__Tim Peters, The Zen of Python'),
+               (u"Flat is better than nested.", u'__Tim Peters, The Zen of Python'),
+               (u"Sparse is better than dense.", u'__Tim Peters, The Zen of Python'),
+               (u"Readability counts.", u'__Tim Peters, The Zen of Python'),
+               (u"Special cases aren't special enough to break the rules.", u'__Tim Peters, The Zen of Python'),
+               (u"Although practicality beats purity.", u'__Tim Peters, The Zen of Python'),
+               (u"Errors should never pass silently, unless explicitly silenced.", u'__Tim Peters, The Zen of Python'),
+               (u"In the face of ambiguity, refuse the temptation to guess.", u'__Tim Peters, The Zen of Python'),
+               (u"Although that way may not be obvious at first unless you're Dutch.", u'__Tim Peters, The Zen of Python'),
+               (u"Now is better than never.", u'__Tim Peters, The Zen of Python'),
+               (u"Although never is often better than *right* now.", u'__Tim Peters, The Zen of Python'),
+               (u"If the implementation is hard to explain, it's a bad idea.", u'__Tim Peters, The Zen of Python'),
+               (u"If the implementation is easy to explain, it may be a good idea.", u'__Tim Peters, The Zen of Python'),
+               (u"Namespaces are one honking great idea -- let's do more of those!", u'__Tim Peters, The Zen of Python'),
+               (u"I once tried Java, but it was too complicated for me, Python is easier.",u'__Valentin Pratz,  novice programmers'),
                ]
-    
+
     shuffle(sayings)
-    
+
     saying, author = sayings[0]
     return saying, author
