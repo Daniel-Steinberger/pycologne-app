2cf48d5506d55b1d336ef8814f93eae839939195
SpotlightKid <bitbucket@chrisarndt.de>
2015-01-15 01:46:43 +0100

    New top-level menu structure and matching handler functions

--- webapp.py
+++ webapp.py
@@ -16,6 +16,9 @@ APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
+# dummy translation function
+_ = lambda s: s
+
 def get_locale():
     # note: PyCologne app currently has DE  only
     return LANGUAGE_SELECTED
@@ -31,12 +34,12 @@ def get_content(filename, overrides=None):
     return content
 
 def get_topmenue():
-    menue = [('/competition', u'Competition'),
-              ('/task', u'Task'),
-              ('/coursematerial', u'Coursematerial'),
-              ('/submission', u'Submission'),
-              ('/prizes', u'Prizes'),
-            ]
+    menue = [
+        ('/about', _(u'Die User Group')),
+        ('/join', _(u'Mitmachen')),
+        ('/events', _(u'Termine')),
+        ('/contact', _(u'Kontakt')),
+    ]
     return menue
 
 app.jinja_env.globals.update(get_topmenue=get_topmenue)
@@ -47,71 +50,34 @@ app.jinja_env.globals.update(get_topmenue=get_topmenue)
 @app.route("/index")
 def index():
     saying, author = get_saying()
-    return render_template("/index.html",
-                           saying=saying,
-                           author=author,
-                           competition_info='About Competition',
-                           dates='Dates',
-                           impressions='Impressions')
+    return render_template("/index.html", saying=saying, author=author)
 # sub pages
 
 # TODO:  add real pycologne content ...
 
-@app.route("/competition")
-def competition():
-    filename = os.path.join("templates", get_locale(), "rst", "competition.rst")
+@app.route("/about")
+def about():
+    filename = os.path.join("templates", get_locale(), "rst", "about.rst")
     content = get_content(filename)
-    return render_template("/content.html", act="competition", content=content)
+    return render_template("/content.html", act="about", content=content)
 
-@app.route("/task")
-def task():
-    filename = os.path.join("templates", get_locale(), "rst", "task.rst")
+@app.route("/join")
+def join():
+    filename = os.path.join("templates", get_locale(), "rst", "join.rst")
     content = get_content(filename)
-    return render_template("/content.html", act="task", content=content)
+    return render_template("/content.html", act="join", content=content)
 
-@app.route("/submission")
-def submission():
-    filename = os.path.join("templates", get_locale(), "rst", "submission.rst")
+@app.route("/events")
+def events():
+    filename = os.path.join("templates", get_locale(), "rst", "events.rst")
     content = get_content(filename)
-    return render_template("/content.html", act="submission", content=content)
+    return render_template("/content.html", act="events", content=content)
 
-@app.route("/coursematerial")
-def coursematerial():
-    filename = os.path.join("templates", get_locale(), "rst", "coursematerial.rst")
+@app.route("/contact")
+def contact():
+    filename = os.path.join("templates", get_locale(), "rst", "contact.rst")
     content = get_content(filename)
-    return render_template("/content.html", act="coursematerial", content=content)
-
-@app.route("/imprint")
-def imprint():
-    filename = os.path.join("templates", get_locale(), "rst", "imprint.rst")
-    content = get_content(filename)
-    return render_template("/content.html", act="imprint", content=content)
-
-@app.route("/privacy")
-def privacy():
-    filename = os.path.join("templates", get_locale(), "rst", "privacy.rst")
-    overrides = {
-                 'initial_header_level': 2,
-                }
-    content = get_content(filename, overrides=overrides)
-    return render_template("/content.html", act="privacy", content=content)
-
-@app.route("/dates")
-def dates():
-    filename = os.path.join("templates", get_locale(), "rst", "dates.rst")
-    content = get_content(filename)
-    return render_template("/content.html",
-                           act="dates", content=content)
-
-@app.route("/prizes")
-def prizes():
-    filename = os.path.join("templates", get_locale(), "rst", "prizes.rst")
-    overrides = {
-                 'initial_header_level': 2,
-                }
-    content = get_content(filename, overrides=overrides)
-    return render_template("/prizes.html",act="prizes", content=content)
-
+    return render_template("/content.html", act="contact", content=content)
 
 # default error handler
 
