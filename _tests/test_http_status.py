b3201d4d3772362732adbfcd408e8f554325ce27
SpotlightKid <bitbucket@chrisarndt.de>
2015-02-20 15:34:44 +0100

    PEP-8 fixes and instantiate webdriver in setUp method to make pylint happy

--- _tests/test_http_status.py
+++ _tests/test_http_status.py
@@ -11,23 +11,26 @@ from selenium.common.exceptions import NoSuchElementException
 class HTTPStatusTest(unittest.TestCase):
     """Test case checking that all URLs return valid pages."""
 
-    def checkURL(self, url):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def check_url(self, url):
         """Check that page content is found."""
         self.browser.get(url)
         try:
-            h1 = self.browser.find_element_by_xpath("//h1")
-            self.assertNotEqual(h1.text, "Page Not Found")
+            heading1 = self.browser.find_element_by_xpath("//h1")
+            self.assertNotEqual(heading1.text, "Page Not Found")
         except NoSuchElementException:
             pass
 
-    def runTest(self):
+    def test_page_status(self):
         """Check all URLs."""
-        self.browser = webdriver.Firefox()
-        path = "http://127.0.0.1:5014/"
+
+        path = "http://localhost:5014/"
         urls = ("", "index", "about", "join", "events", "contact")
 
         for url in urls:
-            self.checkURL(path + url)
+            self.check_url(path + url)
 
 
 if __name__ == "__main__":
