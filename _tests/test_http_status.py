aa1b0edb1a4c118dbb57f5ce674b85a15327bac9
Daniel Steinberger <dst.imap@gmail.com>
2026-05-06 02:24:57 +0200

    Dependencies aufraeumen, Selenium durch Flask-Test-Client ersetzen
    - Production: pytz und six raus (in Python 3.11+ via zoneinfo bzw.
      Python 3 selbst abgedeckt). flask, docutils, babel, python-dateutil
      auf aktuelle Untergrenzen.
    - Dev: selenium, flask-testing, flake8-docstrings, pyflakes raus.
      ruff, mypy und pytest-playwright neu, pytest+pylint auf aktuelle
      Versionen.
    - _tests/test_http_status.py auf Flask-Test-Client umgestellt
      (parametrisiert ueber alle Routen, statt Selenium+LiveServer).
    - uv.lock von uv neu generiert.

--- _tests/test_http_status.py
+++ _tests/test_http_status.py
@@ -1,62 +1,26 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Functional tests for webapp using Selenium."""
-
-from __future__ import unicode_literals, print_function
+"""Smoke-Tests fuer HTTP-Status aller Routen via Flask-Test-Client."""
 
 import os
 
-# we don't use 'from flask.ext.testing import ...'
-# because pylint can't cope with the custom Flask importer
-from flask_testing import LiveServerTestCase
-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+import pytest
 
 from pycgnweb.webapp import app
 
 
-class HTTPStatusTest(LiveServerTestCase):
-    """Test case checking that all URLs return valid pages."""
-
-    def setUp(self):
-        """Create Selenium webdriver instance for Firefox."""
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        """Close the browser window and delete webdriver instance."""
-        self.browser.close()
-        del self.browser
-
-    def create_app(self):
-        """Create Flask application instance."""
-        print("Current dir: %s" % os.getcwd())
-        app.static_folder = os.path.join(os.getcwd(), 'static')
-        app.template_folder = os.path.join(os.getcwd(), 'templates')
-        app.config['TESTING'] = True
-        app.config['LIVESERVER_PORT'] = 5014
-        app.debug = True
-        return app
-
-    def check_url(self, url):
-        """Check that page content is found."""
-        self.browser.get(url)
-        try:
-            heading1 = self.browser.find_element_by_xpath(
-                "//h3[@id='message']")
-            self.assertNotEqual(heading1.text[:15], "URL not found: ")
-        except NoSuchElementException:
-            pass
-
-    def test_page_status(self):
-        """Check all URLs."""
-        path = "http://localhost:5014/"
-        urls = ("", "index", "about", "join", "events", "contact")
-
-        for url in urls:
-            self.check_url(path + url)
+@pytest.fixture
+def client():
+    """Flask-Test-Client mit Pfaden auf das Repo-Root konfiguriert."""
+    app.static_folder = os.path.join(os.getcwd(), "static")
+    app.template_folder = os.path.join(os.getcwd(), "templates")
+    app.config["TESTING"] = True
+    return app.test_client()
 
 
-if __name__ == "__main__":
-    import unittest
-    unittest.main()
+@pytest.mark.parametrize(
+    "path",
+    ["/", "/index", "/about", "/join", "/events", "/contact"],
+)
+def test_page_returns_ok(client, path):
+    """Jede der oeffentlichen Routen muss HTTP 200 liefern."""
+    response = client.get(path)
+    assert response.status_code == 200
